301141746948c45c75b9f6d145d8940ff8d59d84
joaolinux <87071803+joaolinux@users.noreply.github.com>
2022-05-24 14:32:43 -0300

    dev1.py
    Esse código imprime o que você digitar de acordo com o que eu escrevi no código.

---
+++ dev1.py
@@ -0,0 +1,9 @@
+
+# input serve para o usuario digitar, ele sempre retorna uma string
+nome = input ('Escreva seu nome:')
+idade = input ('Escreva sua idade:')
+altura = input ('Digite sua altura')
+
+# Juntei todas variaveis em um print só, para imprimir na tela do usuário
+print(nome, "tem", idade, "anos e", altura, "de altura")
+
